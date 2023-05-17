c1c9ba69173a5014d5746e3d7dbff80c73c7b3a0
Chetan Gurnule <105580754+chetangurnule@users.noreply.github.com>
2023-05-17 21:49:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function volume_sphere() {
     //Write your code here
-  
+	let radius = document.querySelector("#radius").value;
+	let volume = document.querySelector("#volume");
+	let PI = Math.PI;
+	let ans = (4*PI*radius*radius*radius)/ 3;
+	document.querySelector("#volume").value = ans;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
